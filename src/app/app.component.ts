79b7912a7bbe951d04f73ec9023fffdac44a80ef
Subash <Subashtech14@gmail.com>
2023-09-09 16:05:47 +0530

    Routing Explained

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ import { LoggerService } from './logger.service';
 import { localStorageToken } from './localstorage.token';
 import { InitService } from './init.service';
 import { ConfigService } from './rooms/services/config.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
@@ -25,12 +27,26 @@ export class AppComponent implements OnInit {
     @Optional() private loggerService: LoggerService,
     @Inject(localStorageToken) private localStorage: Storage,
     private initService: InitService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private router: Router
   ) {
     console.log(initService.config);
     console.log('Config Service Initialized...');
   }
   ngOnInit(): void {
+    // this.router.events.subscribe((event) => {
+    //   console.log(event);
+    // })
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart),
+    ).subscribe((event) => {
+      console.log('NavigationStart', event);
+    })
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+    ).subscribe((event) => {
+      console.log('Navigation Completed', event);
+    })
     this.loggerService?.log('AppComponent.ngOnInit()');
     // this.name.nativeElement.innerText = 'Hilton Hotel';
     // this.localStorage.setItem('name', 'Subash');
